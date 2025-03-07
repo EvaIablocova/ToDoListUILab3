2828c7990da5871ef6bd963aaf80892e0b449106
EvaIablocova <125370412+EvaIablocova@users.noreply.github.com>
2025-03-07 08:20:47 +0200

    7 March

---
+++ src/main/java/org/example/todolistuilab3/DTOs/TaskStatus.java
@@ -0,0 +1,9 @@
+package org.example.todolistuilab3.DTOs;
+
+public enum TaskStatus {
+    NEW,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED
+}
+
---
+++ src/main/java/org/example/todolistuilab3/ToDoListUiLab3Application.java
@@ -0,0 +1,13 @@
+package org.example.todolistuilab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListUiLab3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListUiLab3Application.class, args);
+    }
+
+}
